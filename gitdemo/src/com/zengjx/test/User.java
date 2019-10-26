c8d915df280189083bc8ec8c77c1f609cd636e53
zengjx <1164512033@qq.com>
2019-10-26 09:14:19 +0800

    新建User.java,提交到本地仓库

---
+++ gitdemo/src/com/zengjx/test/User.java
@@ -0,0 +1,19 @@
+package com.zengjx.test;
+
+/**
+ * @ClassName HelloController
+ * @Description TODO
+ * @Author zengjx
+ * @Company zengjx
+ * @Date 2019/10/26  9:01
+ * @Version V1.0
+ */
+public class User {
+    public static void main(String[] args) {
+
+
+        System.out.println("aaaaaa");
+        System.out.println("asssss");
+
+    }
+}
